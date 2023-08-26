35c2173e6c13921dff2f4bfd97ecb947c977064d
SoonYi <55300935+S00NYI@users.noreply.github.com>
2023-08-26 15:34:40 -0400

    script for CoCLIP enrichment calculation

---
+++ peak_enrichment_calculations.R
@@ -0,0 +1,89 @@
+## CoCLIP Analysis: 
+## Peak Enrichment Calculation
+## Written by Soon Yi
+## Last Edit: 2023-08-25
+
+library(readr)
+library(dplyr)
+
+# peaksMatrix_PATH = 'L:/My Drive/CWRU/PhD/Luna Lab/1. coCLIP/peakMatrix/'  ## Use this for windows machine
+peaksMatrix_PATH = '/Users/soonyi/Desktop/Genomics/CoCLIP/Analysis/'
+peaksMatrix_FILE = 'Combined_peakCoverage_groomed_normalized_annotated.txt'
+
+peakMatrix = read_delim(paste0(peakPATH, peakFILE), show_col_types = FALSE)
+peakMatrix = peakMatrix %>% mutate_at('TOTAL_BC', as.numeric)
+
+pseudoCount = min(peakMatrix[, colnames(peakMatrix)[6:63]][peakMatrix[, colnames(peakMatrix)[6:63]] != 0], na.rm = TRUE)
+peakMatrix[, colnames(peakMatrix)[6:63]] = peakMatrix[, colnames(peakMatrix)[6:63]] + pseudoCount
+
+inert_columns = c('chrom', 'start', 'end', 'peak_names', 'score', 'strand', 
+                  "gene", "annotation", "finalized_annotation", "grouped_annotation", "annotation_count")
+BC_columns = c("Nuc_F_M_BC", "Nuc_F_S_BC", "Cyto_F_M_BC", "Cyto_F_S_BC", 
+               "NLS_I_M_BC", "NLS_I_S_BC", "NES_I_M_BC", "NES_I_S_BC", "G3BP_I_M_BC", "G3BP_I_S_BC",
+               "NLS_E_M_BC", "NLS_E_S_BC", "NES_E_M_BC", "NES_E_S_BC", "G3BP_E_M_BC", "G3BP_E_S_BC",
+               "TOTAL_BC")
+
+Nuc_F_M = c('Nuc_F_M_1', 'Nuc_F_M_2', 'Nuc_F_M_3')
+Nuc_F_S = c('Nuc_F_S_1', 'Nuc_F_S_2', 'Nuc_F_S_3')
+Cyto_F_M = c('Cyto_F_M_1', 'Cyto_F_M_2', 'Cyto_F_M_3')
+Cyto_F_S = c('Cyto_F_S_1', 'Cyto_F_S_2', 'Cyto_F_S_3')
+
+NLS_I_M = c('NLS_I_M_1', 'NLS_I_M_2')
+NLS_I_S = c('NLS_I_S_1', 'NLS_I_S_2')
+NES_I_M = c('NES_I_M_1', 'NES_I_M_2')
+NES_I_S = c('NES_I_S_1', 'NES_I_S_2')
+G3BP_I_M = c('G3BP_I_M_1', 'G3BP_I_M_2', 'G3BP_I_M_3', 'G3BP_I_M_4')
+G3BP_I_S = c('G3BP_I_S_1', 'G3BP_I_S_2', 'G3BP_I_S_3', 'G3BP_I_S_4')
+
+NLS_E_M = c('NLS_E_M_1', 'NLS_E_M_2', 'NLS_E_M_3', 'NLS_E_M_4')
+NLS_E_S = c('NLS_E_S_1', 'NLS_E_S_2', 'NLS_E_S_3', 'NLS_E_S_4')
+NES_E_M = c('NES_E_M_1', 'NES_E_M_2', 'NES_E_M_3', 'NES_E_M_4')
+NES_E_S = c('NES_E_S_1', 'NES_E_S_2', 'NES_E_S_3', 'NES_E_S_4')
+G3BP_E_M = c('G3BP_E_M_1', 'G3BP_E_M_2', 'G3BP_E_M_3', 'G3BP_E_M_4', 'G3BP_E_M_5', 'G3BP_E_M_6')
+G3BP_E_S = c('G3BP_E_S_1', 'G3BP_E_S_2', 'G3BP_E_S_3', 'G3BP_E_S_4', 'G3BP_E_S_5', 'G3BP_E_S_6', 'G3BP_E_S_7')
+
+peakEnrichment = peakMatrix[, inert_columns]
+
+peakEnrichment = peakEnrichment %>% mutate(Nuc_F_M = rowSums(peakMatrix[, Nuc_F_M])/length(Nuc_F_M))
+peakEnrichment = peakEnrichment %>% mutate(Nuc_F_S = rowSums(peakMatrix[, Nuc_F_S])/length(Nuc_F_S))
+peakEnrichment = peakEnrichment %>% mutate(Cyto_F_M = rowSums(peakMatrix[, Cyto_F_M])/length(Cyto_F_M))
+peakEnrichment = peakEnrichment %>% mutate(Cyto_F_S = rowSums(peakMatrix[, Cyto_F_S])/length(Cyto_F_S))
+
+peakEnrichment = peakEnrichment %>% mutate(NLS_I_M = rowSums(peakMatrix[, NLS_I_M])/length(NLS_I_M))
+peakEnrichment = peakEnrichment %>% mutate(NLS_I_S = rowSums(peakMatrix[, NLS_I_S])/length(NLS_I_S))
+peakEnrichment = peakEnrichment %>% mutate(NES_I_M = rowSums(peakMatrix[, NES_I_M])/length(NES_I_M))
+peakEnrichment = peakEnrichment %>% mutate(NES_I_S = rowSums(peakMatrix[, NES_I_S])/length(NES_I_S))
+peakEnrichment = peakEnrichment %>% mutate(G3BP_I_M = rowSums(peakMatrix[, G3BP_I_M])/length(G3BP_I_M))
+peakEnrichment = peakEnrichment %>% mutate(G3BP_I_S = rowSums(peakMatrix[, G3BP_I_S])/length(G3BP_I_S))
+
+peakEnrichment = peakEnrichment %>% mutate(NLS_E_M = rowSums(peakMatrix[, NLS_E_M])/length(NLS_E_M))
+peakEnrichment = peakEnrichment %>% mutate(NLS_E_S = rowSums(peakMatrix[, NLS_E_S])/length(NLS_E_S))
+peakEnrichment = peakEnrichment %>% mutate(NES_E_M = rowSums(peakMatrix[, NES_E_M])/length(NES_E_M))
+peakEnrichment = peakEnrichment %>% mutate(NES_E_S = rowSums(peakMatrix[, NES_E_S])/length(NES_E_S))
+peakEnrichment = peakEnrichment %>% mutate(G3BP_E_M = rowSums(peakMatrix[, G3BP_E_M])/length(G3BP_E_M))
+peakEnrichment = peakEnrichment %>% mutate(G3BP_E_S = rowSums(peakMatrix[, G3BP_E_S])/length(G3BP_E_S))
+
+peakEnrichment = cbind(peakEnrichment, peakMatrix[, BC_columns])
+write.table(peakEnrichment, paste0(peaksMatrix_PATH, str_replace(peaksMatrix_FILE, ".txt", "_enrichment.txt")), 
+            quote = FALSE, col.names = TRUE, row.names = FALSE, sep = '\t', na = "")
+
+
+
+## snoRNA Comparison ##
+snoRNA_Enrichment = peakEnrichment %>% filter(finalized_annotation == 'snoRNA')
+
+## Filter by BC
+snoRNA_Enrichment1 = snoRNA_Enrichment %>% filter(Nuc_F_M_BC >= 3, Cyto_F_M_BC >= 3, NLS_E_M_BC >= 4, NES_E_M_BC >= 4)
+
+xlims = c(0.6, 1.2)
+ylims = c(0.6, 1.2)
+
+snoRNA_F_M_NvC = log(snoRNA_Enrichment1$Nuc_F_M, 2) / log(snoRNA_Enrichment1$Cyto_F_M, 2)
+snoRNA_E_M_NvC = log(snoRNA_Enrichment1$NLS_E_M, 2) / log(snoRNA_Enrichment1$NES_E_M, 2)
+plot(snoRNA_E_M_NvC, snoRNA_F_M_NvC, xlim = xlims, ylim = ylims, xlab = "LOG2(CoCLIP NLS/NES Mock)", ylab = "LOG2(FractionCLIP Nuc/Cyto Mock)", pch = 16, col = "blue",)
+cor(snoRNA_E_M_NvC, snoRNA_F_M_NvC)
+
+snoRNA_F_S_NvC = log(snoRNA_Enrichment1$Nuc_F_S, 2) / log(snoRNA_Enrichment1$Cyto_F_S, 2)
+snoRNA_E_S_NvC = log(snoRNA_Enrichment1$NLS_E_S, 2) / log(snoRNA_Enrichment1$NES_E_S, 2)
+points(snoRNA_E_S_NvC, snoRNA_F_S_NvC, xlim = xlims, ylim = ylims, xlab = "LOG2(CoCLIP NLS/NES Stress)", ylab = "LOG2(FractionCLIP Nuc/Cyto Stress)", pch = 16, col = "red",)
+cor(snoRNA_E_S_NvC, snoRNA_F_S_NvC)
