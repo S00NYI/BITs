8a70063c236f5fc3c6566054c8866a5ad169086d
SoonYi <55300935+S00NYI@users.noreply.github.com>
2023-10-25 21:26:22 -0400

    bash script using modified fastx_barcode_splitter.pl

--- CLIPittyCLIP_BITs/demultiplex_FASTQ_Calls.sh
+++ CLIPittyCLIP_BITs/demultiplex_FASTQ_Calls.sh
@@ -1,23 +1,63 @@
-cd ..
 cd JL0361
-stripBarcode.pl -len 7 -format fastq -v JL0361_Input.fastq JL0361_Input_rmBC.fastq
-cat JL0361_Input_rmBC.fastq | fastx_barcode_splitter.pl --bcfile JL0361_Input_BC.txt --bol --mismatches 2 --prefix "JL0361_Input_" --suffix ".fastq"
-stripBarcode.pl -len 7 -format fastq -v JL0361_Enrich.fastq JL0361_Enrich_rmBC.fastq
-cat JL0361_Enrich_rmBC.fastq | fastx_barcode_splitter.pl --bcfile JL0361_Enrich_BC.txt --bol --mismatches 2 --prefix "JL0361_Enrich_" --suffix ".fastq"
+gunzip -k *.gz
+cat JL0361_Input.fastq | perl fastx_barcode_splitter_custom.pl --bcfile JL0361_Input_BC.txt --bol --mismatches 2 --prefix "JL0361_Input_" --suffix ".fastq"
+cat JL0361_Enrich.fastq | perl fastx_barcode_splitter_custom.pl --bcfile JL0361_Enrich_BC.txt --bol --mismatches 2 --prefix "JL0361_Enrich_" --suffix ".fastq"
+rm JL0361_Input.fastq
+rm JL0361_Enrich.fastq
+mkdir demultiplexed
+mv *.fastq demultiplexed
+cd demultiplexed
+gzip -k *.fastq
+mkdir fastq_gz
+mkdir fastq_raw
+mv *.gz fastq_gz
+mv *.fastq fastq_raw
+cd ..
 
 cd ..
 cd JL0380
-stripBarcode.pl -len 7 -format fastq -v JL0380_Fraction.fastq JL0380_Fraction_rmBC.fastq
-cat JL0380_Fraction_rmBC.fastq | fastx_barcode_splitter.pl --bcfile JL0380_Fraction_BC.txt --bol --mismatches 2 --prefix "JL0380_Fraction_" --suffix ".fastq"
+gunzip -k *.gz
+cat JL0380_Fraction.fastq | perl fastx_barcode_splitter_custom.pl --bcfile JL0380_Fraction_BC.txt --bol --mismatches 2 --prefix "JL0380_Fraction_" --suffix ".fastq"
+rm JL0380_Fraction.fastq
+mkdir demultiplexed
+mv *.fastq demultiplexed
+cd demultiplexed
+gzip -k *.fastq
+mkdir fastq_gz
+mkdir fastq_raw
+mv *.gz fastq_gz
+mv *.fastq fastq_raw
+cd ..
 
 cd ..
 cd JL0388
-stripBarcode.pl -len 7 -format fastq -v JL0388_Input.fastq JL0388_Input_rmBC.fastq
-cat JL0388_Input_rmBC.fastq | fastx_barcode_splitter.pl --bcfile JL0388_Input_BC.txt --bol --mismatches 2 --prefix "JL0388_Input_" --suffix ".fastq"
-stripBarcode.pl -len 7 -format fastq -v JL0388_Enrich.fastq JL0388_Enrich_rmBC.fastq
-cat JL0388_Enrich_rmBC.fastq | fastx_barcode_splitter.pl --bcfile JL0388_Enrich_BC.txt --bol --mismatches 2 --prefix "JL0388_Enrich_" --suffix ".fastq"
+gunzip -k *.gz
+cat JL0388_Input.fastq | perl fastx_barcode_splitter_custom.pl --bcfile JL0388_Input_BC.txt --bol --mismatches 2 --prefix "JL0388_Input_" --suffix ".fastq"
+cat JL0388_Enrich.fastq | perl fastx_barcode_splitter_custom.pl --bcfile JL0388_Enrich_BC.txt --bol --mismatches 2 --prefix "JL0388_Enrich_" --suffix ".fastq"
+rm JL0388_Input.fastq
+rm JL0388_Enrich.fastq
+mkdir demultiplexed
+mv *.fastq demultiplexed
+cd demultiplexed
+gzip -k *.fastq
+mkdir fastq_gz
+mkdir fastq_raw
+mv *.gz fastq_gz
+mv *.fastq fastq_raw
+cd ..
 
 cd ..
 cd JL1024
-stripBarcode.pl -len 7 -format fastq -v JL1024_Pool.fastq JL1024_Pool_rmBC.fastq
-cat JL1024_Pool_rmBC.fastq | fastx_barcode_splitter.pl --bcfile JL1024_Pool_BC.txt --bol --mismatches 2 --prefix "JL1024_Pool_" --suffix ".fastq"
+gunzip -k *.gz
+cat JL1024_Pool.fastq | perl fastx_barcode_splitter_custom.pl --bcfile JL1024_Pool_BC.txt --bol --mismatches 2 --prefix "JL1024_Pool_" --suffix ".fastq"
+rm JL1024_Pool.fastq
+mkdir demultiplexed
+mv *.fastq demultiplexed
+cd demultiplexed
+gzip -k *.fastq
+mkdir fastq_gz
+mkdir fastq_raw
+mv *.gz fastq_gz
+mv *.fastq fastq_raw
+cd ..
+cd ..
