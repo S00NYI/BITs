81df744b73a63ef9652176e86ae78707ae79ec46
SoonYi <55300935+S00NYI@users.noreply.github.com>
2024-01-06 03:13:40 -0500

    removed deprecated parts

--- CLIPittyCLIP_BITs/bedgraph_normalization.R
+++ CLIPittyCLIP_BITs/bedgraph_normalization.R
@@ -1,7 +1,7 @@
 ## CoCLIP Analysis: 
 ## BedGraph Normalization by Mapped Read Depth
 ## Written by Soon Yi
-## Last Edit: 2023-09-27
+## Last Edit: 2024-01-06
 ## Use this after running normalized_bedgraph.sh
 
 library(tidyr)
@@ -40,32 +40,3 @@ for (bgFile in bgFiles) {
   write.table(bg, paste0(baseDir, '/finished_bedgraph/', out_bg), row.names = F, col.names = F, quote = F, append = T)
 }
 
-
-
-
-# exp_IDs = c('JL0361_Input', 'JL0361_Enrich', 'JL0380_Fraction', 'JL0388_Input', 'JL0388_Enrich', 'JL1024_Pool')
-# 
-# mapped_read_depth = t(read.table('/Users/soonyi/Desktop/Genomics/CoCLIP/Analysis/Combined_peakCoverage_mappedDepth.txt', header = T))
-# colnames(mapped_read_depth) = 'depth'
-# 
-# for (exp_ID in exp_IDs) {
-#   bgDir = paste0(baseDir, str_split(exp_ID, '_')[[1]][1], '/', exp_ID, '/', exp_ID, '_mapped_reads/BEDGRAPH/')
-#   setwd(bgDir)
-#   bgFiles = list.files()
-#   bgNames = unique(sapply(strsplit(bgFiles, '\\.'), '[', 1))
-#   
-#   for (bgName in bgNames) {
-#     depth = mapped_read_depth[bgName, ]
-#     pos_bgDir = paste0(bgDir, bgName, '.sorted.collapsed.pos.bedgraph')
-#     rev_bgDir = paste0(bgDir, bgName, '.sorted.collapsed.rev.bedgraph')
-#     
-#     pos_bgDir = read.table(pos_bgDir)
-#     rev_bgDir = read.table(rev_bgDir)
-#     
-#     pos_bgDir$V4 = as.integer(round(pos_bgDir$V4/depth*1e6))
-#     rev_bgDir$V4 = as.integer(round(rev_bgDir$V4/depth*1e6))
-#     
-#     write.table(pos_bgDir, paste0(baseDir, 'Analysis/normalized_BEDGRAPH/', bgName, '.sorted.collapsed.pos.normalized.bedgraph'), row.names = F, col.names = F, quote = F)
-#     write.table(rev_bgDir, paste0(baseDir, 'Analysis/normalized_BEDGRAPH/', bgName, '.sorted.collapsed.rev.normalized.bedgraph'), row.names = F, col.names = F, quote = F)
-#   }
-# }
--- CLIPittyCLIP_BITs/bedgraph_normalization.sh
+++ CLIPittyCLIP_BITs/bedgraph_normalization.sh
@@ -15,18 +15,6 @@ while IFS=$'\t' read -r NAME DEPTH; do
     done
 done < "$depth_file"
 
-# depth_file="Combined_peakCoverage_mappedDepth.txt"
-
-# while IFS=$'\t' read -r NAME DEPTH; do
-#     for id in "/Users/soonyi/Desktop/Genomics/CoCLIP/combined_collapsedBED/collapsedBED/${NAME}.sorted.collapsed.bed"; do
-#         echo "Running intersect to filter to peaks..."
-#         bedtools intersect -s -wa -a "${id}" -b peaks_Sorted_Filtered.bed > ./filtered_bed/$(basename "$id" .bed).filtered.bed
-#         echo "Running genomecov for ./filtered_bed/$(basename "$id" .bed).filtered.bed with normalization factor ${DEPTH}..."
-#         bedtools genomecov -i ./filtered_bed/$(basename "$id" .bed).filtered.bed -g /Users/soonyi/Desktop/Genomics/Annotations/GRCh38.primary_assembly.genome.fa.fai -bg -strand + > ./normalized_bedgraph/$(basename "$id" .bed).filtered.pos.bedgraph
-#         bedtools genomecov -i ./filtered_bed/$(basename "$id" .bed).filtered.bed -g /Users/soonyi/Desktop/Genomics/Annotations/GRCh38.primary_assembly.genome.fa.fai -bg -strand - > ./normalized_bedgraph/$(basename "$id" .bed).filtered.rev.bedgraph
-#     done
-# done < "$depth_file"
-
 
 # Input Mock
 bedtools unionbedg -i ./normalized_bedgraph/JL0361_Input_G3BP_Input_Mock*.sorted.collapsed.filtered.pos.bedgraph ./normalized_bedgraph/JL0388_Input_NLS_Input_Mock*.sorted.collapsed.filtered.pos.bedgraph ./normalized_bedgraph/JL0388_Input_NES_Input_Mock*.sorted.collapsed.filtered.pos.bedgraph ./normalized_bedgraph/JL0388_Input_SG_Input_Mock*.sorted.collapsed.filtered.pos.bedgraph ./normalized_bedgraph/JL1024_Pool_NLS_I_Mock*.sorted.collapsed.filtered.pos.bedgraph ./normalized_bedgraph/JL1024_Pool_NES_I_Mock*.sorted.collapsed.filtered.pos.bedgraph ./normalized_bedgraph/JL1024_Pool_G3BP_I_Mock*.sorted.collapsed.filtered.pos.bedgraph -header > ./normalized_combined_bedgraph/CoCLIP_Input_Mock.combined.filtered.pos.bedgraph
@@ -76,35 +64,3 @@ bedtools unionbedg -i ./normalized_bedgraph/JL0380_Fraction_Cyto_Fraction_Ars*.s
 
 ## Proceed to bedgraph_normalization.R
 
-
-
-## Deprecated
-# cat JL0361_Input_G3BP_Input_Mock*.bed JL0388_Input_SG_Input_Mock*.bed JL1024_Pool_G3BP_I_Mock*.bed JL0388_Input_NES_Input_Mock*.bed JL1024_Pool_NES_I_Mock*.bed JL0388_Input_NLS_Input_Mock*.bed JL1024_Pool_NLS_I_Mock*.bed > CoCLIP_Input_Mock.bed
-# cat JL0361_Input_G3BP_Input_Ars*.bed JL0388_Input_SG_Input_Ars*.bed JL1024_Pool_G3BP_I_Ars*.bed JL0388_Input_NES_Input_Ars*.bed JL1024_Pool_NES_I_Ars*.bed JL0388_Input_NLS_Input_Ars*.bed JL1024_Pool_NLS_I_Ars*.bed > CoCLIP_Input_Arsenite.bed
-
-# cat JL0388_Enrich_NLS_Enrich_Mock*.bed JL1024_Pool_NLS_E_Mock*.bed > CoCLIP_NLS_Mock.bed
-# cat JL0388_Enrich_NLS_Enrich_Ars*.bed JL1024_Pool_NLS_E_Ars*.bed > CoCLIP_NLS_Arsenite.bed
-
-# cat JL0388_Enrich_NES_Enrich_Mock*.bed JL1024_Pool_NES_E_Mock*.bed > CoCLIP_NES_Mock.bed
-# cat JL0388_Enrich_NES_Enrich_Ars*.bed JL1024_Pool_NES_E_Ars*.bed > CoCLIP_NES_Arsenite.bed
-
-# cat JL0361_Enrich_G3BP_Enrich_Mock*.bed JL0388_Enrich_SG_Enrich_Mock*.bed JL1024_Pool_G3BP_E_Mock*.bed > CoCLIP_G3BP_Mock.bed
-# cat JL0361_Enrich_G3BP_Enrich_Ars*.bed JL0388_Enrich_SG_Enrich_Ars*.bed JL1024_Pool_G3BP_E_Ars*.bed > CoCLIP_G3BP_Arsenite.bed
-
-# cat JL0380_Fraction_Nuc_Fraction_Mock*.bed > FracCLIP_Nuclear_Mock.bed
-# cat JL0380_Fraction_Nuc_Fraction_Ars*.bed > FracCLIP_Nuclear_Arsenite.bed
-# cat JL0380_Fraction_Cyto_Fraction_Mock*.bed > FracCLIP_Cytoplasm_Mock.bed
-# cat JL0380_Fraction_Cyto_Fraction_Ars*.bed > FracCLIP_Cytoplasm_Arsenite.bed
-
-# sort -k 1,1 -k2,2n CoCLIP_Input_Mock.bed > CoCLIP_Input_Mock.sorted.bed
-# sort -k 1,1 -k2,2n CoCLIP_Input_Arsenite.bed > CoCLIP_Input_Arsenite.sorted.bed
-# sort -k 1,1 -k2,2n CoCLIP_NLS_Mock.bed > CoCLIP_NLS_Mock.sorted.bed
-# sort -k 1,1 -k2,2n CoCLIP_NLS_Arsenite.bed > CoCLIP_NLS_Arsenite.sorted.bed
-# sort -k 1,1 -k2,2n CoCLIP_NES_Mock.bed > CoCLIP_NES_Mock.sorted.bed
-# sort -k 1,1 -k2,2n CoCLIP_NES_Arsenite.bed > CoCLIP_NES_Arsenite.sorted.bed
-# sort -k 1,1 -k2,2n CoCLIP_G3BP_Mock.bed > CoCLIP_G3BP_Mock.sorted.bed
-# sort -k 1,1 -k2,2n CoCLIP_G3BP_Arsenite.bed > CoCLIP_G3BP_Arsenite.sorted.bed
-# sort -k 1,1 -k2,2n FracCLIP_Nuclear_Mock.bed > FracCLIP_Nuclear_Mock.sorted.bed
-# sort -k 1,1 -k2,2n FracCLIP_Nuclear_Arsenite.bed > FracCLIP_Nuclear_Arsenite.sorted.bed
-# sort -k 1,1 -k2,2n FracCLIP_Cytoplasm_Mock.bed > FracCLIP_Cytoplasm_Mock.sorted.bed
-# sort -k 1,1 -k2,2n FracCLIP_Cytoplasm_Arsenite.bed > FracCLIP_Cytoplasm_Arsenite.sorted.bed
