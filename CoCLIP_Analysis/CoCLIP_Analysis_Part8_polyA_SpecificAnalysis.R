625b54d0665ddf98fe99e201617f5c45dd5192cb
SoonYi <55300935+S00NYI@users.noreply.github.com>
2024-02-09 12:14:50 -0500

    polyA peak analysis

---
+++ CoCLIP_Analysis/CoCLIP_Analysis_Part8_polyA_SpecificAnalysis.R
@@ -0,0 +1,282 @@
+## CoCLIP Analysis: 
+## polyA specific Analysis
+## Written by Soon Yi
+## Last Edit: 2024-02-05
+
+library(stringr)
+library(readr)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(data.table)
+library(scales)
+library(GenomicRanges)
+library(IRanges)
+library(BSgenome)
+library(BSgenome.Hsapiens.UCSC.hg38)
+library(Biostrings)
+
+## Load peak matrix and clean up:
+####################################################################################################################
+peaksMatrix_PATH = '/Users/soonyi/Desktop/Genomics/CoCLIP/Analysis/'
+peaksMatrix_FILE = 'Combined_peakCoverage_groomed_normalized_annotated.txt'
+
+peaksMatrix = read_delim(paste0(peaksMatrix_PATH, peaksMatrix_FILE), show_col_types = FALSE)
+peaksMatrix = peaksMatrix %>% mutate_at('TOTAL_BC', as.numeric)
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'unannotated', 'UnAn', finalized_annotation))
+peaksMatrix = peaksMatrix %>% mutate(grouped_annotation = ifelse(grouped_annotation == 'unannotated', 'UnAn', grouped_annotation))
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'downstream 10K', 'DS10K', finalized_annotation))
+peaksMatrix = peaksMatrix %>% mutate(grouped_annotation = ifelse(grouped_annotation == 'downstream 10K', 'DS10K', grouped_annotation))
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'ncRNA_Retained_intron', 'nC_RI', finalized_annotation))
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'CDS_Retained_intron', 'CDS_RI', finalized_annotation))
+
+## further consolidate ncRNA:
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'miRNA', 'Other', finalized_annotation))
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'scaRNA', 'Other', finalized_annotation))
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'nC_RI', 'Other', finalized_annotation))
+peaksMatrix = peaksMatrix %>% mutate(finalized_annotation = ifelse(finalized_annotation == 'CDS_RI', 'intron', finalized_annotation))
+
+## Column organization:
+inert_columns = c('chrom', 'start', 'end', 'peak_names', 'score', 'strand', 
+                  "gene", "external_gene_name", "annotation", "finalized_annotation", "grouped_annotation", "annotation_count", "TOTAL_TagCount")
+BC_columns = c("Nuc_F_M_BC", "Nuc_F_S_BC", "Cyto_F_M_BC", "Cyto_F_S_BC", 
+               "NLS_I_M_BC", "NLS_I_S_BC", "NES_I_M_BC", "NES_I_S_BC", "G3BP_I_M_BC", "G3BP_I_S_BC",
+               "NLS_E_M_BC", "NLS_E_S_BC", "NES_E_M_BC", "NES_E_S_BC", "G3BP_E_M_BC", "G3BP_E_S_BC",
+               "TOTAL_BC")
+
+Nuc_F_M = c('Nuc_F_M_1', 'Nuc_F_M_2', 'Nuc_F_M_3')
+Nuc_F_S = c('Nuc_F_S_1', 'Nuc_F_S_2', 'Nuc_F_S_3')
+Cyto_F_M = c('Cyto_F_M_1', 'Cyto_F_M_2', 'Cyto_F_M_3')
+Cyto_F_S = c('Cyto_F_S_1', 'Cyto_F_S_2', 'Cyto_F_S_3')
+
+NLS_I_M = c('NLS_I_M_1', 'NLS_I_M_2')
+NES_I_M = c('NES_I_M_1', 'NES_I_M_2')
+G3BP_I_M = c('G3BP_I_M_1', 'G3BP_I_M_2', 'G3BP_I_M_3', 'G3BP_I_M_4')
+
+NLS_I_S = c('NLS_I_S_1', 'NLS_I_S_2')
+NES_I_S = c('NES_I_S_1', 'NES_I_S_2')
+G3BP_I_S = c('G3BP_I_S_1', 'G3BP_I_S_2', 'G3BP_I_S_3', 'G3BP_I_S_4', 'G3BP_I_S_5')
+
+NLS_E_M = c('NLS_E_M_1', 'NLS_E_M_2', 'NLS_E_M_3', 'NLS_E_M_4')
+NLS_E_S = c('NLS_E_S_1', 'NLS_E_S_2', 'NLS_E_S_3', 'NLS_E_S_4')
+NES_E_M = c('NES_E_M_1', 'NES_E_M_2', 'NES_E_M_3', 'NES_E_M_4')
+NES_E_S = c('NES_E_S_1', 'NES_E_S_2', 'NES_E_S_3', 'NES_E_S_4')
+G3BP_E_M = c('G3BP_E_M_1', 'G3BP_E_M_2', 'G3BP_E_M_3', 'G3BP_E_M_4', 'G3BP_E_M_5', 'G3BP_E_M_6')
+G3BP_E_S = c('G3BP_E_S_1', 'G3BP_E_S_2', 'G3BP_E_S_3', 'G3BP_E_S_4', 'G3BP_E_S_5', 'G3BP_E_S_6', 'G3BP_E_S_7')
+
+## Add row sum columns for further filtering:
+peaksMatrix$F_rowSum = rowSums(peaksMatrix[, c(Nuc_F_M, Nuc_F_S, Cyto_F_M, Cyto_F_S)])
+peaksMatrix$I_rowSum = rowSums(peaksMatrix[, c(NLS_I_M, NLS_I_S, NES_I_M, NES_I_S, G3BP_I_M, G3BP_I_S)])
+peaksMatrix$E_rowSum = rowSums(peaksMatrix[, c(NLS_E_M, NLS_E_S, NES_E_M, NES_E_S, G3BP_E_M, G3BP_E_S)])
+rowSum_columns = c('F_rowSum', 'I_rowSum', 'E_rowSum')
+
+## Add pseudocount:
+pseudoCount = min(peaksMatrix[, colnames(peaksMatrix)[6:63]][peaksMatrix[, colnames(peaksMatrix)[6:63]] != 0], na.rm = TRUE)
+peaksMatrix[, colnames(peaksMatrix)[6:63]] = peaksMatrix[, colnames(peaksMatrix)[6:63]] + pseudoCount
+
+####################################################################################################################
+
+## Custom Functions 
+####################################################################################################################
+## Filter peaks based on the designated criteria:
+filterPeakMatrix = function(peak_matrix, sample_list, info_columns, BC_criteria, rowSum_criteria = NULL) {
+  temp = peak_matrix[, c(info_columns, sample_list)]
+  
+  if (!is.null(rowSum_criteria)) {
+    temp$rowAvg = rowSums(temp[, sample_list]) / length(sample_list)
+    temp = temp[(rowSums(temp[, paste0(unique(sub("_[0-9]+$", "", sample_list)), '_BC')]) >= BC_criteria) & (temp$rowAvg > (median(temp$rowAvg) * rowSum_criteria)), ]
+    temp$rowAvg = NULL
+  } else {
+    temp = temp[(rowSums(temp[, paste0(unique(sub("_[0-9]+$", "", sample_list)), '_BC')]) >= BC_criteria), ]
+  }
+  
+  return(temp)
+}
+
+## Plot Scatter:
+plotScatter = function(peak_matrix, annotation_level, 
+                       x_axis, y_axis, 
+                       x_label = NULL, y_label = NULL, 
+                       x_lim = NULL, y_lim = NULL, 
+                       x_cnt_lim = NULL, y_cnt_lim = NULL, 
+                       title = NULL, diag = FALSE) {
+  
+  plot = ggplot(peak_matrix, aes(x = ({{x_axis}}), y = ({{y_axis}}), color = {{annotation_level}})) +
+    geom_point(pch = 16, size = 3, alpha = 0.5) +
+    scale_fill_brewer(palette = "Set3") +
+    theme_bw() + 
+    theme(axis.text = element_text(size=14), 
+          axis.title = element_text(size=14, face = 'bold'), 
+          legend.text = element_text(size=14))
+  
+  if (!is.null(title)) {
+    plot = plot + ggtitle(title)
+  }
+  if (!is.null(x_label)) {
+    plot = plot + labs(x = x_label)
+  }
+  if (!is.null(y_label)) {
+    plot = plot + labs(y = y_label)
+  }
+  if (!is.null(x_lim)) {
+    plot = plot + xlim(x_lim)
+  }
+  if (!is.null(y_lim)) {
+    plot = plot + ylim(y_lim)
+  }
+  if (diag == TRUE) {
+    plot = plot + geom_abline(linetype = 'dotted') 
+  }
+  # Stacked density plot for x-axis
+  x_densities = ggplot(peak_matrix, aes(x = {{x_axis}}, fill = {{annotation_level}})) +
+    geom_density(aes(y = ..count..), alpha = 0.7) +
+    theme_bw() + 
+    theme(axis.text = element_text(size=14), 
+          axis.title = element_text(size=14, face = 'bold'), 
+          legend.position = 'none') +
+    xlim(x_lim)
+  
+  if (!is.null(x_cnt_lim)) {
+    x_densities = x_densities + ylim(x_cnt_lim)
+  } else {
+    x_densities = x_densities + ylim(c(0, NA))
+  }
+  
+  # Stacked density plot for y-axis
+  y_densities = ggplot(peak_matrix, aes(x = {{y_axis}}, fill = {{annotation_level}})) +
+    geom_density(aes(y = ..count..), alpha = 0.7) +
+    theme_bw() + 
+    theme(axis.text = element_text(size=14), 
+          axis.title = element_text(size=14, face = 'bold'), 
+          legend.position = 'none') +
+    coord_flip() +
+    xlim(x_lim)
+  
+  if (!is.null(y_cnt_lim)) {
+    y_densities = y_densities + ylim(y_cnt_lim)
+  } else {
+    y_densities = y_densities + ylim(c(0, NA))
+  }
+  
+  
+  empty_plot = ggplot() + theme_void()
+  
+  # Arrange the plots
+  plot = gridExtra::grid.arrange(
+    x_densities, empty_plot,
+    plot, y_densities,
+    ncol = 2,
+    nrow = 2,
+    widths = c(4, 1),
+    heights = c(1, 4)
+  )
+  
+  return(plot)
+}
+####################################################################################################################
+
+## Filter Criteria:
+####################################################################################################################
+BC_Threshold_F = 2
+BC_Threshold_I = 4
+BC_Threshold_E = 2
+BC_Threshold_E_SG = 3
+
+rowSum_Multiplier_F = 4
+rowSum_Multiplier_I = 2
+rowSum_Multiplier_E = 2
+####################################################################################################################
+
+## Subset of Peaks for downstream analysis:
+####################################################################################################################
+## Fractionation CLIP
+Peak_F_Nuc_M = filterPeakMatrix(peaksMatrix, Nuc_F_M, c(inert_columns, BC_columns), BC_Threshold_F, rowSum_Multiplier_F)
+Peak_F_Nuc_S = filterPeakMatrix(peaksMatrix, Nuc_F_S, c(inert_columns, BC_columns), BC_Threshold_F, rowSum_Multiplier_F)
+Peak_F_Cyt_M = filterPeakMatrix(peaksMatrix, Cyto_F_M, c(inert_columns, BC_columns), BC_Threshold_F, rowSum_Multiplier_F)
+Peak_F_Cyt_S = filterPeakMatrix(peaksMatrix, Cyto_F_S, c(inert_columns, BC_columns), BC_Threshold_F, rowSum_Multiplier_F)
+
+## CoCLIP
+Peak_Co_Input_M = filterPeakMatrix(peaksMatrix, c(NLS_I_M, NES_I_M, G3BP_I_M), c(inert_columns, BC_columns), BC_Threshold_I, rowSum_Multiplier_I)
+Peak_Co_Input_S = filterPeakMatrix(peaksMatrix, c(NLS_I_S, NES_I_S, G3BP_I_S), c(inert_columns, BC_columns), BC_Threshold_I, rowSum_Multiplier_I)
+Peak_Co_NLS_M = filterPeakMatrix(peaksMatrix, NLS_E_M, c(inert_columns, BC_columns), BC_Threshold_E, rowSum_Multiplier_E)
+Peak_Co_NLS_S = filterPeakMatrix(peaksMatrix, NLS_E_S, c(inert_columns, BC_columns), BC_Threshold_E, rowSum_Multiplier_E)
+Peak_Co_NES_M = filterPeakMatrix(peaksMatrix, NES_E_M, c(inert_columns, BC_columns), BC_Threshold_E, rowSum_Multiplier_E)
+Peak_Co_NES_S = filterPeakMatrix(peaksMatrix, NES_E_S, c(inert_columns, BC_columns), BC_Threshold_E, rowSum_Multiplier_E)
+Peak_Co_G3BP_M = filterPeakMatrix(peaksMatrix, G3BP_E_M, c(inert_columns, BC_columns), BC_Threshold_E_SG, rowSum_Multiplier_E)
+Peak_Co_G3BP_S = filterPeakMatrix(peaksMatrix, G3BP_E_S, c(inert_columns, BC_columns), BC_Threshold_E_SG, rowSum_Multiplier_E)
+####################################################################################################################
+
+## Global Variables:
+####################################################################################################################
+CoCLIP_List = c('Co_M_Input', 'Co_S_Input', 'Co_M_NLS', 'Co_S_NLS', 'Co_M_NES', 'Co_S_NES', 'Co_M_G3BP', 'Co_S_G3BP')
+F_CoCLIP_List = c('F_M_Nuc', 'F_M_Cyt', 'Co_M_NLS', 'Co_M_NES', 'F_S_Nuc', 'F_S_Cyt', 'Co_S_NLS', 'Co_S_NES')
+
+All_Annotation_List = c("5'UTR", "CDS", "3'UTR", "intron", "snoRNA", 'ncRNA', "TE", "Other", "DS10K")
+mRNA_List = c("5'UTR", "CDS", "3'UTR", "intron", 'CDS_RI')
+ncRNA_List = c('rRNA', 'lncRNA', 'tRNA', 'snRNA', 'snoRNA', 'TE', 'Other')
+####################################################################################################################
+
+## :
+####################################################################################################################
+extension = 65
+
+# Convert peak to GRanges object:
+Peak_Temp = Peak_Co_G3BP_S
+Peak_Temp = Peak_Temp %>% mutate(chrom = case_when(chrom == 'chrMT' ~ 'chrM', TRUE ~ as.character(chrom)))
+Peak_Temp = Peak_Temp %>% filter(chrom %in% c('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 
+                                              'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY', 'chrM'))
+Peak_Temp = Peak_Temp %>% mutate(start = as.numeric(start))
+Peak_Temp = Peak_Temp %>% mutate(end = as.numeric(end))
+
+Peak_Temp$start = Peak_Temp$start - extension
+Peak_Temp$end = Peak_Temp$end + extension
+
+Peak_GR = GRanges(Peak_Temp)
+
+# Get sequence of peaks
+Peak_Seqs = getSeq(BSgenome.Hsapiens.UCSC.hg38, Peak_GR, as.character = TRUE)
+Peak_Seqs = DNAStringSet(Peak_Seqs)
+
+PolyA_IDs = elementNROWS(vmatchPattern('AAAAA', Peak_Seqs)) > 0
+
+PolyA_Peak_GR = Peak_GR[PolyA_IDs]
+
+PolyA_Peak_DF = as.data.frame(PolyA_Peak_GR)
+
+
+UTR3_gene_list = data.frame(unique((PolyA_Peak_DF %>% filter(grouped_annotation == "3'UTR"))$external_gene_name))
+colnames(UTR3_gene_list) = c('gene')
+
+# write_csv(UTR3_gene_list, './list.csv')
+
+shared = data.frame(gene = 
+                      unique(PolyA_Peak_DF_NES$external_gene_name)[unique(PolyA_Peak_DF_NES$external_gene_name) %in% unique(PolyA_Peak_DF_G3BP$external_gene_name)])
+
+
+table(PolyA_Peak_DF$grouped_annotation)
+
+####################################################################################################################
+
+## :
+####################################################################################################################
+####################################################################################################################
+
+## :
+####################################################################################################################
+####################################################################################################################
+
+## :
+####################################################################################################################
+####################################################################################################################
+
+## :
+####################################################################################################################
+####################################################################################################################
+
+
+
+####################################################################################################################
+
+
+
+####################################################################################################################
